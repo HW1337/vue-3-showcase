502393168643508a729cf8aa4d900ba8be93b16d
HW1337 <kskobelin@ya.ru>
2023-11-10 18:55:29 +0700

    implement dynamic rendering of user posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
-        <h3>Список пользователей</h3>
+    <div v-if="posts.length > 0">
+        <h3>Список постов</h3>
         <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"/>
     </div>
+    <h3 v-else> Список пуст </h3>
 </template>
 
 <script>
