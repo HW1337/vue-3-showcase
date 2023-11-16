32a86a6ceaa37640538275ce6c98fe59d2df5f0a
HW1337 <kskobelin@ya.ru>
2023-11-16 20:04:21 +0700

    added animations for adding and removing posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,9 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"/>
+        <transition-group name="post-list">
+            <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"/>
+        </transition-group>
     </div>
     <h3 v-else> Список пуст </h3>
 </template>
@@ -20,5 +22,20 @@ import PostItem from "@/components/PostItem";
 </script>
 
 <style scoped>
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
